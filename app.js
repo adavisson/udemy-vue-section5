1160a5fe633516ceabc591fed7a6666726655094
Andrew Davisson <akdavisson4@gmail.com>
2020-06-03 10:51:23 -0600

    understanding $refs

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 let data = {
   title: 'The VueJS Instance',
-  showParagraph: false
+  showParagraph: false,
 }
 
 let vm1 = new Vue({
   el: '#app1',
   data: data,
   methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
+    show: function () {
+      this.showParagraph = true
+      this.updateTitle('The VueJS Instance (Updated)')
+      this.$refs.myButton.innerText = 'Test'
     },
-    updateTitle: function(title) {
-      this.title = title;
-    }
+    updateTitle: function (title) {
+      this.title = title
+    },
   },
   computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
+    lowercaseTitle: function () {
+      return this.title.toLowerCase()
+    },
   },
   watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
+    title: function (value) {
+      alert('Title changed, new value: ' + value)
+    },
+  },
 })
 
 console.log(vm1.$data === data)
+vm1.$refs.heading.innerText = "Something Else"
 
 setTimeout(() => {
   vm1.title = 'Changed By Timer'
@@ -37,11 +39,11 @@ setTimeout(() => {
 let vm2 = new Vue({
   el: '#app2',
   data: {
-    title: "The second instance"
+    title: 'The second instance',
   },
   methods: {
     onChange() {
       vm1.title = 'Changed!'
-    }
-  }
+    },
+  },
 })
